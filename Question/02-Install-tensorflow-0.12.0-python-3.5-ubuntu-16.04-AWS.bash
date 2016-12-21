1a12112c6f98e0dbbe9403ff634eaa3c4fe86b53
Moheb <mhbashari@gmail.com>
2016-12-21 14:23:38 +0330

    Create 02-Install-tensorflow-0.12.0-python-3.5-ubuntu-16.04-AWS.bash

---
+++ Question/02-Install-tensorflow-0.12.0-python-3.5-ubuntu-16.04-AWS.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Thanks to https://github.com/mGalarnyk/Installations_Mac_Ubuntu_Windows/blob/master/TensorFlow/Python3.5_setup_aws_tensorflow.bash
+# install the required packages
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get -y install linux-headers-$(uname -r) linux-image-extra-`uname -r`
+sudo apt-get install python3-numpy python3-dev python3-wheel
+
+# install cuda
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+rm cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+sudo apt-get update
+sudo apt-get install -y cuda
+
+# get cudnn
+# Register and download the cuDNN v4 from https://developer.nvidia.com/rdp/cudnn-download
+CUDNN_FILE=cudnn-8.0-linux-x64-v5.1.tgz
+wget http://developer.download.nvidia.com/compute/redist/cudnn/v4/${CUDNN_FILE}
+tar xvzf ${CUDNN_FILE}
+rm ${CUDNN_FILE}
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include # move library files to /usr/local/cuda
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+rm -rf cuda
+
+# set the appropriate library path
+echo 'export CUDA_HOME=/usr/local/cuda
+export CUDA_ROOT=/usr/local/cuda
+export PATH=$PATH:$CUDA_ROOT/bin:$HOME/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
+' >> ~/.bashrc
+
+# Ubuntu/Linux 64-bit, GPU enabled, Python 3.5 tensorflow
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.0-cp35-cp35m-linux_x86_64.whl
+
+pip3 install $TF_BINARY_URL
+
+# install monitoring programs
+sudo wget https://git.io/gpustat.py -O /usr/local/bin/gpustat
+sudo chmod +x /usr/local/bin/gpustat
+sudo nvidia-smi daemon
+sudo apt-get -y install htop
+
+# update terminal to latest update of .bashrc file
+source .bashrc
+exec bash
