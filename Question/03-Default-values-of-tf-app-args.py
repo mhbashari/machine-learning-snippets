7563f30633d8e7126aa415509a90cdea149bedbc
Moheb <mhbashari@gmail.com>
2016-12-26 15:54:19 +0330

    Update 03-Default-values-of-tf-app-args.py

--- Question/03-Default-values-of-tf-app-args.py
+++ Question/03-Default-values-of-tf-app-args.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
 flags = tf.app.flags
 flags.FLAGS._parse_flags()
-print(("\n".join(flags.FLAGS.__dict__['__flags'].keys())))
+for key, value in flags.FLAGS.__dict__['__flags'].items():
+    print(key, value)
